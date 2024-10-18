0bd6f7340cfe5933021c7aabe639ffe5b5426de9
Pedro Ballester <pedroballester@RT6601ML-GGB.local>
2024-10-18 15:13:47 -0400

    Fixes

--- otterapiwrapperR/R/utils.R
+++ otterapiwrapperR/R/utils.R
@@ -1,3 +1,10 @@
+#' Local plots
+#'
+#' Use plotly to create local plots.
+#'
+#' @param plot_data Result from the plotting functions from otter.
+#' @return Plots.
+#' @export
 otter_to_plot <- function (plot_data) {
   return(plotly_build(plot_data))
 }
